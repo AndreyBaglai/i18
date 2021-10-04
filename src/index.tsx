e70051b7aff48ae54d2b3db528769fcc1a360c1e
AndreyBaglai <andreybaglai@gmail.com>
2021-10-04 10:04:39 +0300

    feat: add packages

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import './index.css';
+import 'normalize.css';
+import 'sources/styles/styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
